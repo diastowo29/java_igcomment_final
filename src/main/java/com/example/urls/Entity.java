e4e2965d50fc91fb2282ed7c2b34a00614621b92
diastowo29 <diastowo@gmail.com>
2019-03-27 14:40:59 +0700

    change url manifest etc

--- src/main/java/com/example/urls/Entity.java
+++ src/main/java/com/example/urls/Entity.java
@@ -2,8 +2,8 @@ package com.example.urls;
 
 public class Entity {
 
-	public String HEROKUDOMAIN = "https://java-cif-instagram.herokuapp.com/";
-	public String CALLBACKURL = "https://java-cif-instagram.herokuapp.com/instagram/callback";
+	public String HEROKUDOMAIN = "https://java-igcomment-gpay.herokuapp.com/";
+	public String CALLBACKURL = HEROKUDOMAIN + "instagram/callback";
 	public String FB_API_DOMAIN = "https://graph.facebook.com/v3.0";
 	public String GET_ACC_ID_API = FB_API_DOMAIN + "/me/accounts?fields=connected_instagram_account,name&access_token=";
 	
