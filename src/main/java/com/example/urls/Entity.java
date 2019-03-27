a05edff442c8e57f4a0c945dc71ad1ac59d15196
diastowo29 <diastowo@gmail.com>
2019-03-27 15:04:13 +0700

    change gpay to gtech:

--- src/main/java/com/example/urls/Entity.java
+++ src/main/java/com/example/urls/Entity.java
@@ -2,7 +2,7 @@ package com.example.urls;
 
 public class Entity {
 
-	public String HEROKUDOMAIN = "https://java-igcomment-gpay.herokuapp.com/";
+	public String HEROKUDOMAIN = "https://java-igcomment-gtech.herokuapp.com/";
 	public String CALLBACKURL = HEROKUDOMAIN + "instagram/callback";
 	public String FB_API_DOMAIN = "https://graph.facebook.com/v3.0";
 	public String GET_ACC_ID_API = FB_API_DOMAIN + "/me/accounts?fields=connected_instagram_account,name&access_token=";
