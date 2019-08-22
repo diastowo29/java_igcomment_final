969c0be9a5db60724478ad1b61e86ac230d71f86
diastowo29 <diastowo@gmail.com>
2019-08-22 13:40:27 +0700

    change api version

--- src/main/java/com/example/urls/Entity.java
+++ src/main/java/com/example/urls/Entity.java
@@ -4,7 +4,7 @@ public class Entity {
 
 	public String HEROKUDOMAIN = "https://java-ig-comment-dev.herokuapp.com/";
 	public String CALLBACKURL = HEROKUDOMAIN + "instagram/callback";
-	public String FB_API_DOMAIN = "https://graph.facebook.com/v3.0";
+	public String FB_API_DOMAIN = "https://graph.facebook.com/v3.3";
 	public String GET_ACC_ID_API = FB_API_DOMAIN + "/me/accounts?fields=connected_instagram_account,name&access_token=";
 	
 	public int defaultInterval = 2;
