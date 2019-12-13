02bd438b97e0745a9ef9763f9be10701daf3b4bc
diastowo29 <diastowo@gmail.com>
2019-12-13 20:45:24 +0700

    what change?

--- src/main/java/com/example/Instagram.java
+++ src/main/java/com/example/Instagram.java
@@ -1,8 +1,10 @@
 package com.example;
 
 import java.io.UnsupportedEncodingException;
+import java.net.InetAddress;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -261,6 +263,22 @@ public class Instagram {
 
 		hashMap.put("urls", urlMap);
 		return new ResponseEntity<Object>(hashMap, HttpStatus.OK);
+	}
+
+	@RequestMapping("/testing")
+	ResponseEntity<Object> testingOnly() {
+		InetAddress hostname;
+		try {
+			hostname = InetAddress.getLocalHost();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		HashMap<String, String> newHash = new HashMap<>();
+		newHash.put("hostname", hostname);
+
+		return new ResponseEntity<Object>(newHash, HttpStatus.OK);
 	}
 
 	/* FIXME CHANNELBACK */
